a13c5a0891f4d9cd12c49d09e4fc5b6f7ed45818
Helge Stegemoen <helge@stegemoen.com>
2025-06-26 22:49:19 +0200

    3:16 Render the site logo image and fixed problem with loadEnv by using dotenv instead

--- astro-estates-course/astro.config.mjs
+++ astro-estates-course/astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+//import {loadEnv} from 'vite';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd());
+const imageDomain = process.env.PUBLIC_IMAGE_DOMAIN || "";
+console.log('IMAGE_DOMAIN: ', imageDomain);
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains:[imageDomain]
+  },
   vite: {
     plugins: [tailwindcss()]
   }
